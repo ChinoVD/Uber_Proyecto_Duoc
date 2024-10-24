88db59d0124999f5f3c0a296de92db7e3adce7f5
Ignaico <ign.cornejoz@duocuc.cl>
2024-10-23 21:49:10 -0300

    V.1.2.9

--- src/app/cuenta/cuenta.page.ts
+++ src/app/cuenta/cuenta.page.ts
@@ -11,12 +11,29 @@ export class CuentaPage implements OnInit {
   constructor(
     private routing: Router
   ) { }
+  nombre: string = '';
 
-  navegar(){
+  navegarAjustes(){
     this.routing.navigate(['/ajustes'])
   }
 
   ngOnInit() {
-  }
+    // Obtener datos del local storage
+    const usuarioGuardado = localStorage.getItem('usuarios');
+    const idUsuarioActual = localStorage.getItem('idUsuarioActual'); // Obtener el ID almacenado
+  
+    if (usuarioGuardado && idUsuarioActual) {
+      const usuarios = JSON.parse(usuarioGuardado); 
+      // Buscar el usuario con el ID correspondiente
+      const usuarioActual = usuarios.find((user: any) => user.id.toString() === idUsuarioActual);
+  
+      if (usuarioActual) {
+        this.nombre = usuarioActual.usuario || '';
+      } else {
+        console.log("Usuario no encontrado.");
+      }
+    } else {
+      console.log("No hay usuarios guardados o no hay sesión activa.");
+    }}
 
 }
